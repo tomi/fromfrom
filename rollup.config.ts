f19ddfaf68cf4f6fc18d1ba25e746655e13221a9
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-03-12 20:39:07 +0200

    style: fix code style in rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,24 +1,29 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import camelCase from "lodash.camelcase";
+import typescript from "rollup-plugin-typescript2";
+import json from "rollup-plugin-json";
 
-const pkg = require('./package.json')
+const pkg = require("./package.json");
 
-const libraryName = 'fromfrom'
+const libraryName = "fromfrom";
 
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: "umd",
+      sourcemap: true,
+    },
+    { file: pkg.module, format: "es", sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
-    include: 'src/**',
+    include: "src/**",
   },
   plugins: [
     // Allow json resolution
@@ -35,4 +40,4 @@ export default {
     // Resolve source maps to the original source
     sourceMaps(),
   ],
-}
+};
